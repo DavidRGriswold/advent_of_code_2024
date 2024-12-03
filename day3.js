07d090def4655afe3cfcf44f42049340a392a67c
David Griswold <novachild@gmail.com>
2024-12-03 11:05:38 -0300

    day 3 format fix

--- day3.js
+++ day3.js
@@ -6,10 +6,11 @@ var fs = require('fs'),
 
 let lines = fs.readFileSync(filePath).toString();
 let r = /mul\((\d+),(\d+)\)/g;
+
 let all = lines.matchAll(r);
 let sum = 0;
 for (let match of all) {
-    sum += Number(match[1])*Number(match[2]);
+    sum += Number(match[1]) * Number(match[2]);
 }
 console.log(sum)
 
@@ -17,6 +18,7 @@ r = /do\(\)|don't\(\)|mul\((\d+),(\d+)\)/g;
 all = lines.matchAll(r);
 sum = 0;
 let enabled = true;
+
 for (let match of all) {
     console.log(match[0]);
     if (match[0]=="do()") enabled = true;
