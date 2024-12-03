8c5e51150196798d84fd6ba3e5302bb6f7170b2c
David Griswold <novachild@gmail.com>
2024-12-03 11:06:05 -0300

    day 3 format fix again

--- day3.js
+++ day3.js
@@ -1,8 +1,8 @@
 var fname = process.argv[2];
 console.log(fname);
-var fs = require('fs'),
-    path = require('path'),
-    filePath = path.join(__dirname, fname);
+var fs = require("fs"),
+  path = require("path"),
+  filePath = path.join(__dirname, fname);
 
 let lines = fs.readFileSync(filePath).toString();
 let r = /mul\((\d+),(\d+)\)/g;
@@ -10,9 +10,9 @@ let r = /mul\((\d+),(\d+)\)/g;
 let all = lines.matchAll(r);
 let sum = 0;
 for (let match of all) {
-    sum += Number(match[1]) * Number(match[2]);
+  sum += Number(match[1]) * Number(match[2]);
 }
-console.log(sum)
+console.log(sum);
 
 r = /do\(\)|don't\(\)|mul\((\d+),(\d+)\)/g;
 all = lines.matchAll(r);
@@ -20,9 +20,9 @@ sum = 0;
 let enabled = true;
 
 for (let match of all) {
-    console.log(match[0]);
-    if (match[0]=="do()") enabled = true;
-    else if (match[0]=="don't()") enabled = false;
-    else if (enabled) sum += Number(match[1])*Number(match[2]);
+  console.log(match[0]);
+  if (match[0] == "do()") enabled = true;
+  else if (match[0] == "don't()") enabled = false;
+  else if (enabled) sum += Number(match[1]) * Number(match[2]);
 }
-console.log(sum)
+console.log(sum);
