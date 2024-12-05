7bb0bd185957cafba4c026e9f6cb1c9993a127d5
David Griswold <novachild@gmail.com>
2024-12-05 10:12:23 -0300

    comments and simplifications

--- day1simplified.js
+++ day1simplified.js
@@ -5,7 +5,7 @@
 
 /* These first 7 lines simply read the input into a single string called input */
 var fname = process.argv[2];
-console.log(fname);
+console.log("Running with input data " + fname);
 var fs = require('fs'),
     path = require('path'),
     filePath = path.join(__dirname, fname);
@@ -14,39 +14,50 @@ let input = fs.readFileSync(filePath).toString();
 
 // split input into an array of strings, on per line
 let lines = input.split("\n");
+
+// create two empty lists
 let l1 = [], l2 = [];
 
 // iterate through each line
-for (let line of lines) {
+for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
     // split the line by spaces, so t is an array of strings that
-    // hold numeric values
+    // hold numeric values like ["2", "11"]
     let t = line.split(/\W+/);
-    // create the two lists, l1 is column 1, l2 is column 2
-    // convert them into numeric values as we go
-    l1.push(Number(t[0]));
-    l2.push(Number(t[1]));
+    // convert the strings to numbers and add them to the lists
+    l1[i] = Number(t[0]);
+    l2[i] = Number(t[1]);
 }
-// yay sort
+
+// sort the lists
 l1.sort();
 l2.sort();
 
-let diffs = [];
+// keep track of the sum of the differences
 let sum = 0;
 for (let i  = 0 ; i < l1.length; i++) {
     // push the difference into an array called diffs
-    diffs.push(Math.abs(l1[i]-l2[i]));
-    sum+= diffs[i];
+    let diff = Math.abs(l1[i] - l2[i]);
+    sum += diff;
 }
+
 // answer to part 1
 console.log(sum);
 
-let sim = 0;
-for (let num of l1) {
-    // this filter method creates an array of all
-    // of the elements of list that equal num
-    // so the length returns how many l2s match each value from l1
-    sim += num * l2.filter((val)=>(val===num)).length;
-}
-// answer to part 2
-console.log(sim);
+// start part 2 now
 
+let simValue = 0;
+// this For Each loop lets us iterate through l1 without needing an index
+for (let num of l1) {
+    //count how many elements in l2 == num
+    let count = 0;
+    for (let i = 0; i < l2.length; i++) {
+        if (l2[i] == num) {
+            count++;
+        }
+    }
+    simValue += num * count;
+}
+
+//answer to part 2
+console.log(simValue);
--- day2.js
+++ day2.js
@@ -1,16 +1,33 @@
+/* To run this code, you need to have nodejs installed on your computer.
+ * You can then run the command `node day2.js data2.txt` to run your code on
+ * input stored inside a file called data2.txt
+ */
+
+/* These first 7 lines simply read the input into a single string called input */
+
 var fname = process.argv[2];
 console.log(fname);
 var fs = require('fs'),
     path = require('path'),
     filePath = path.join(__dirname, fname);
 
-let lines = fs.readFileSync(filePath).toString().split("\n");
+let input = fs.readFileSync(filePath).toString();
+let lines = input.split("\n");
+
+// converst line from an array of strings to an array of numeric arrays, using 
+// the split and map functions
 lines = lines.map((e)=>e.split(/\W+/).map(Number));
+
+// lines.map(safe) creates an array of true/false based on whether each line is safe
+// then .filter ((e)=>e) removes the false statements
+// then .length tells us how many trues. So these two lines are the answers to
+// parts 1 and 2
 
 console.log(lines.map(safe).filter((e)=>e).length);
 console.log(lines.map(pdSafe).filter((e)=>e).length);
 
-/** @param {Array<Number>} row */
+/**  Returns true if a line is safe by part 1 definition
+ * @param {Array<Number>} row */
 function safe(row) {
     let dir = Math.sign(row[1]-row[0]);
     for (let i = 0; i < row.length-1; i++) {
@@ -20,7 +37,8 @@ function safe(row) {
     return true;
 }
 
-/** @param {Array<Number>} row */
+/** Returns true if a line is safe by part 2 definition 
+ * @param {Array<Number>} row */
 function pdSafe(row) {
     if (safe(row)) return true;
     for (let i = 0; i < row.length; i++) {
