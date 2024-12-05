62cb47732c0ba49edbacaf1013b4f7d9258100d1
David Griswold <novachild@gmail.com>
2024-12-05 06:44:48 -0300

    formats

--- day5.js
+++ day5.js
@@ -1,23 +1,25 @@
 var fname = process.argv[2];
 console.log(fname);
 var fs = require("fs"),
-  path = require("path"),
-  filePath = path.join(__dirname, fname);
+    path = require("path"),
+    filePath = path.join(__dirname, fname);
 
 let input = fs.readFileSync(filePath).toString();
 let [ruleStrings, updates] = input.split("\n\n");
-let rules = {}
+let rules = {};
+
 // Build a rule object where the first page is the key, value is an array
 // of required followup objects
-for (let e of ruleStrings.split("\n").map((e)=>e.split("|").map(Number))) {
+for (let e of ruleStrings.split("\n").map((e) => e.split("|").map(Number))) {
     if (rules[e[0]]) rules[e[0]].push(e[1]);
     else rules[e[0]] = [e[1]];
 }
 
 //updates to numbers
-updates = updates.split("\n").map((e)=>e.split(",").map(Number));
+updates = updates.split("\n").map((e) => e.split(",").map(Number));
 let correctUpdates = [];
-let incorrectUpdates = []
+let incorrectUpdates = [];
+
 // split into correct and incorrect
 for (let u of updates) {
     if (isCorrect(u)) correctUpdates.push(u.slice());
@@ -28,13 +30,13 @@ for (let u of updates) {
 console.log(midSum(correctUpdates));
 
 //part 2
-incorrectUpdates.forEach((e)=>e.sort(orderFunction));
+incorrectUpdates.forEach((e) => e.sort(orderFunction));
 console.log(midSum(incorrectUpdates));
 
 function isCorrect(update) {
-    for (let i = update.length-1; i > 0; i--) {
+    for (let i = update.length - 1; i > 0; i--) {
         if (rules[update[i]]) {
-            for (let j = i -1; j >=0; j--) {
+            for (let j = i - 1; j >= 0; j--) {
                 if (rules[update[i]].includes(update[j])) return false;
             }
         }
@@ -45,7 +47,7 @@ function isCorrect(update) {
 function midSum(updates) {
     let sum = 0;
     for (let u of updates) {
-        sum += u[(u.length-1)/2];
+        sum += u[(u.length - 1) / 2];
     }
     return sum;
 }
