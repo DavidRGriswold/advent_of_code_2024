9cef7c4632a1c9e42a0e2dc78a3b8291b75bd977
David Griswold <novachild@gmail.com>
2024-12-05 10:26:11 -0300

    additional day2 comments

--- day2simplified.js
+++ day2simplified.js
@@ -35,11 +35,15 @@ for (let line of lines) {
 /**  Returns true if a line is safe by part 1 definition
  * @param {Array<Number>} row */
 function safe(row) {
+    // find the direction - this will give +1 for increasing and -1 for decreasing
     let dir = Math.sign(row[1]-row[0]);
     for (let i = 0; i < row.length-1; i++) {
+        // this will always result in a positive number IF the direction
+        // is constant (so if its negative, we are not safe!)
         let diff = dir * (row[i+1] - row[i])
         if (diff < 1 || diff > 3) return false;
     }
+    // if the loop finishes without returning, we are safe
     return true;
 }
 
@@ -49,7 +53,10 @@ function pdSafe(row) {
     if (safe(row)) return true;
     for (let i = 0; i < row.length; i++) {
         // toSpliced used this way makes a copy with an element removed
+        // so we can just check if the array minus this element is safe
+        // if ANY of them are safe, then the thing is safe so return true
         if (safe(row.toSpliced(i,1))) return true;
     }
+    // if the loop finishes without returning, we are NOT safe
     return false;
 }
