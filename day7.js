ed22aa9b36e19e2f656ced0e47ff7fcf32ff4ba4
David Griswold <novachild@gmail.com>
2024-12-07 22:21:36 -0300

    faster day 7

--- day7.js
+++ day7.js
@@ -15,7 +15,7 @@ for (let i = 0; i < goals.length; i++) {
   if (works(goals[i], vals[i], 1, vals[i][0], false)) sum += goals[i];
 }
 console.log(sum); // part 1
-console.log("Part 1 in " + (Date.now()-startTime) + " ms");
+console.log("Part 1 in " + (Date.now() - startTime) + " ms");
 
 // start part 2
 sum = 0;
@@ -29,10 +29,10 @@ console.log("Both parts in " + (Date.now() - startTime) + " ms");
  * Recursively checks if a working solution can be found
  * @param {Number} goal The desired value
  * @param {Number[]} vals The values
- * @param {Number} i The index of the next thing to process 
+ * @param {Number} i The index of the next thing to process
  * @param {Number} current Accumulated value
  * @param {boolean} allowCat whether to allow concatenation
- * @returns 
+ * @returns
  */
 function works(goal, vals, i, current, allowCat) {
   let thisVal = vals[i];
@@ -40,16 +40,18 @@ function works(goal, vals, i, current, allowCat) {
   if (i == vals.length - 1) {
     let b = current + thisVal == goal || current * thisVal == goal;
     if (b || !allowCat) return b;
-    return (
-      Number(current + "" + thisVal) == goal ||
-      Number(current + "" + thisVal) == goal
-    );
+    return concat(current, thisVal) == goal || concat(current, thisVal) == goal;
   }
   let b =
     works(goal, vals, i + 1, current + thisVal, allowCat) ||
     works(goal, vals, i + 1, current * thisVal, allowCat);
   if (b || !allowCat) return b;
-  return works(goal, vals, i + 1, Number(current + "" + thisVal), allowCat);
+  return works(goal, vals, i + 1, concat(current, thisVal), allowCat);
+}
+
+function concat(a, b) {
+  let digits = Math.floor(Math.log10(b)) + 1;
+  return a * Math.pow(10,digits) + b;
 }
 
 function parseInput(input) {
